146512f52db0f07138107d72a3b25d1a23f3b43e
Jwatts82 <wattscodes@gmail.com>
2021-10-06 12:58:33 -0700

    convert to functional components

--- src/components/events/EventItem.js
+++ src/components/events/EventItem.js
@@ -1,34 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { PropTypes } from 'prop-types'
 
-class EventItem extends Component {
-    render() {
-        const { service_style, img_url } = this.props.event;
-        
-        return (
-            <div className='card text-center'>
-                <img src={img_url} alt="" style={{ width: '60px' }}
-                />
+const EventItem = ({event: { service_style, img_url }}) => {
+    return (
+        <div className='card text-center'>
+            <img src={img_url} alt="" style={{ width: '60px' }}
+            />
             <h3>{service_style}</h3>
 
             <div>
                 <h3>Calculate</h3>  
             </div>
-                {/* <img
-                    src={img_url}
-                    alt=''
-                    className='round-img'
-                    style={{ width: '60px' }}
-                />
-                <h3>{style}</h3>
-                
-                <div>
-                    <h3>
-                        Calculate
-                    </h3>
-                </div> */}
-            </div>
-        )
-    }
+        </div>
+    )
+}
+
+EventItem.propTypes = {
+    event: PropTypes.object.isRequired,
 }
 
 export default EventItem
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export class navbar extends Component {
-    render() {
-        return (
-            <nav className="navbar">
-                <h1>Staffing Calculator</h1>
-            </nav>
-        )
-    }
+const navbar = (props) => {
+    return (
+        <nav className="navbar">
+            <h1>Staffing Calculator</h1>
+        </nav>
+    );
 }
+
+
 
 export default navbar
